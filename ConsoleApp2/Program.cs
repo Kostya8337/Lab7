ff133cb5800fc5ef51b4981c4c923621996b33fd
Костя Осадчий <qwrhevsjjdb83736@gmail.com>
2024-12-16 21:27:50 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    // Універсальний метод для обміну значеннями
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main(string[] args)
+    {
+        double x = 5.5, y = 10.2;
+        Console.WriteLine($"До обміну: x = {x}, y = {y}");
+        Swap(ref x, ref y);
+        Console.WriteLine($"Після обміну: x = {x}, y = {y}\n");
+
+        string str1 = "Hello", str2 = "World";
+        Console.WriteLine($"До обміну: str1 = {str1}, str2 = {str2}");
+        Swap(ref str1, ref str2);
+        Console.WriteLine($"Після обміну: str1 = {str1}, str2 = {str2}");
+    }
+}
